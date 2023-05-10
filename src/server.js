178cec378b28cbd36ad9b050a37a74b935b40c93
Milli-saved <million12.tenkir@gmail.com>
2023-05-10 14:01:02 +0300

    Write packages to be downloaded.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const TelegramBot = require("telegram-bot-api");
 
+
+// must install telegram-bot-api, cors, bcrypt, dotenv, express-async-handler, jsonwebtoken, mongoose,
+// devdependencies nodemon
 const api = new TelegramBot({ token: "My token from bot father" });
 
 api.on("message", (message) => {
