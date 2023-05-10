225582b739120ea635c0c4c434e567320f17b5b5
Milli-saved <million12.tenkir@gmail.com>
2023-05-10 14:50:57 +0300

    Setup server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
+const express = requrie("express");
+const cors = require("cors");
 const TelegramBot = require("telegram-bot-api");
 
+const app = express();
 
+app.use(cors());
 // must install telegram-bot-api, cors, bcrypt, dotenv, express-async-handler, jsonwebtoken, mongoose,
 // devdependencies nodemon
 const api = new TelegramBot({ token: "My token from bot father" });
 
 api.on("message", (message) => {
   console.log("received messages", message);
+});
+
+app.listen(3000, () => {
+  console.log("Port started at 3000");
 });
