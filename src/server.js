c60fb53b83eb30aefc49261235fe50aeee6abab1
Milli-saved <million12.tenkir@gmail.com>
2023-05-18 10:28:13 +0300

    Add dummy blogs to the server and setup the routes.

--- src/server.js
+++ src/server.js
@@ -7,14 +7,79 @@ dotenv.config();
 const token = process.env.TELEGRAM_TOKEN;
 const app = express();
 
+let blogs = [
+  {
+    id: 1,
+    postTitle: "This is the title.",
+    postSubtitle:
+      "This is the subtitle which contains the best part of the blog.",
+    postedBy: "Akrem Hussen",
+    postDate: "on July 8, 2023",
+  },
+  {
+    id: 2,
+    postTitle: "This is the title.",
+    postSubtitle:
+      "This is the subtitle which contains the best part of the blog.",
+    postedBy: "Akrem Hussen",
+    postDate: "on July 8, 2023",
+  },
+  {
+    id: 3,
+    postTitle: "This is the title.",
+    postSubtitle:
+      "This is the subtitle which contains the best part of the blog.",
+    postedBy: "Akrem Hussen",
+    postDate: "on July 8, 2023",
+  },
+  {
+    id: 4,
+    postTitle: "This is the title.",
+    postSubtitle:
+      "This is the subtitle which contains the best part of the blog.",
+    postedBy: "Akrem Hussen",
+    postDate: "on July 8, 2023",
+  },
+  {
+    id: 5,
+    postTitle: "This is the title.",
+    postSubtitle:
+      "This is the subtitle which contains the best part of the blog.",
+    postedBy: "Akrem Hussen",
+    postDate: "on July 8, 2023",
+  },
+  {
+    id: 6,
+    postTitle: "This is the title.",
+    postSubtitle:
+      "This is the subtitle which contains the best part of the blog.",
+    postedBy: "Akrem Hussen",
+    postDate: "on July 8, 2023",
+  },
+];
+
 console.log("telegram bot api", token);
 
 app.use(cors());
-const api = new TelegramBot(token, {
-  polling: true,
+// const api = new TelegramBot(token, {
+//   polling: true,
+// });
+// api.on("message", (message) => {
+//   console.log("received messages", message);
+// });
+
+app.use("/").get((req, res) => {
+  res.status(200).json(blogs);
 });
-api.on("message", (message) => {
-  console.log("received messages", message);
+
+app.use("/:id").get((req, res) => {
+  let id = req.params.id;
+  let selectedBlog = blogs.map((eachBlog) => {
+    if (eachBlog.id === id) {
+      return eachBlog;
+    }
+  });
+  res.status(200).json(selectedBlog);
 });
 
 app.listen(3000, () => {
